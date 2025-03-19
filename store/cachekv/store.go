fd81c84843320062d9b09c3a0244274a66cb6280
violet <violet@hypha.coop>
2025-03-19 13:32:43 -0400

    store

--- store/cachekv/store.go
+++ store/cachekv/store.go
@@ -32,6 +32,10 @@ type Store struct {
 	parent        types.KVStore
 }
 
+type PooledStore struct {
+	Store
+}
+
 var _ types.CacheKVStore = (*Store)(nil)
 
 // NewStore creates a new Store object
@@ -42,6 +46,30 @@ func NewStore(parent types.KVStore) *Store {
 		sortedCache:   internal.NewBTree(),
 		parent:        parent,
 	}
+}
+
+var storePool = sync.Pool{
+	New: func() any {
+		return &PooledStore{
+			Store: Store{
+				cache:         make(map[string]*cValue),
+				unsortedCache: make(map[string]struct{}),
+				sortedCache:   internal.NewBTree(),
+			},
+		}
+	},
+}
+
+func (store *PooledStore) Release() {
+	store.resetCaches()
+	store.parent = nil
+	storePool.Put(store)
+}
+
+func NewPooledStore(parent types.KVStore) *PooledStore {
+	store := storePool.Get().(*PooledStore)
+	store.parent = parent
+	return store
 }
 
 // GetStoreType implements Store.
@@ -112,7 +140,7 @@ func (store *Store) resetCaches() {
 			delete(store.unsortedCache, key)
 		}
 	}
-	store.sortedCache = internal.NewBTree()
+	store.sortedCache.Clear()
 }
 
 // Implements Cachetypes.KVStore.
@@ -121,7 +149,7 @@ func (store *Store) Write() {
 	defer store.mtx.Unlock()
 
 	if len(store.cache) == 0 && len(store.unsortedCache) == 0 {
-		store.sortedCache = internal.NewBTree()
+		store.sortedCache.Clear()
 		return
 	}
 
